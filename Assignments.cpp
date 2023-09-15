6605dcaed5ae764c9ce4ded12e6343e0fa1b2bf2
RAJVARDHAN-M <84311650+RAJVARDHAN-M@users.noreply.github.com>
2023-09-15 12:23:27 +0530

    fifth commit

--- Assignments.cpp
+++ Assignments.cpp
@@ -49,3 +49,63 @@ public:
                 break;
         }
     }
+
+    // Move backward in the current direction
+    void mvBd() {
+        switch (currentOrientation) {
+            case North:
+                currentPosition.yPos--;
+                break;
+            case South:
+                currentPosition.yPos++;
+                break;
+            case East:
+                currentPosition.xPos--;
+                break;
+            case West:
+                currentPosition.xPos++;
+                break;
+            case Up:
+                currentPosition.zPos--;
+                break;
+            case Down:
+                currentPosition.zPos++;
+                break;
+        }
+    }
+
+    // Rotate left (change direction counterclockwise)
+    void rotateLeft() {
+        switch (currentOrientation) {
+            case North:
+                currentOrientation = West;
+                break;
+            case South:
+                currentOrientation = East;
+                break;
+            case East:
+                currentOrientation = North;
+                break;
+            case West:
+                currentOrientation = South;
+                break;
+        }
+    }
+
+    // Rotate right (change direction clockwise)
+    void rotateRight() {
+        switch (currentOrientation) {
+            case North:
+                currentOrientation = East;
+                break;
+            case South:
+                currentOrientation = West;
+                break;
+            case East:
+                currentOrientation = South;
+                break;
+            case West:
+                currentOrientation = North;
+                break;
+        }
+    }
