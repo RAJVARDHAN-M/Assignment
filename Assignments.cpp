8bc057c09cf72140ff76d8e401dec04e3dc28e68
RAJVARDHAN-M <84311650+RAJVARDHAN-M@users.noreply.github.com>
2023-09-15 11:49:21 +0530

    first commit

--- Assignments.cpp
+++ Assignments.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 // Define a structure to represent coordinates in 3D space
-struct Coordinate {
-    int x, y, z;
+struct SpacePoint {
+    int xPos, yPos, zPos;
 };
 
 // Define an enumeration for cardinal directions and 3D orientation
@@ -14,5 +14,14 @@ enum Direction { North, South, East, West, Up, Down };
 // Create a class to simulate a 3D spacecraft
 class Spacecraft {
 private:
-    Coordinate position;   // Store the current position (x, y, z)
-    Direction facing;      // Store the current direction or orientation
+    SpacePoint currentPosition;   // Store the current position (x, y, z)
+    Direction currentOrientation; // Store the current direction or orientation
+
+public:
+    // Constructor to initialize the spacecraft's position and direction
+    Spacecraft(int initialX, int initialY, int initialZ, Direction initialDirection) {
+        currentPosition.xPos = initialX;
+        currentPosition.yPos = initialY;
+        currentPosition.zPos = initialZ;
+        currentOrientation = initialDirection;
+    }
