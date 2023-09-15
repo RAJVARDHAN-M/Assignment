9bf3db32bfc89c7d2ac4718e079ae92975fe9cf7
RAJVARDHAN-M <84311650+RAJVARDHAN-M@users.noreply.github.com>
2023-09-15 13:07:27 +0530

    sixth commit

--- Assignments.cpp
+++ Assignments.cpp
@@ -109,3 +109,84 @@ public:
                 break;
         }
     }
+     // Rotate upward (change direction to Up)
+    void rotateUpward() {
+        currentOrientation = Up;
+    }
+
+    // Rotate downward (change direction to Down)
+    void rotateDownward() {
+        currentOrientation = Down;
+    }
+
+    // Execute a sequence of commands
+    void executeCommands(const vector<char>& commands) {
+        for (char command : commands) {
+            switch (command) {
+                case 'f':
+                    mvFd();
+                    break;
+                case 'b':
+                    mvBd();
+                    break;
+                case 'l':
+                    rotateLeft();
+                    break;
+                case 'r':
+                    rotateRight();
+                    break;
+                case 'u':
+                    rotateUpward();
+                    break;
+                case 'd':
+                    rotateDownward();
+                    break;
+            }
+        }
+    }
+
+    // Display the final position and direction of the spacecraft
+    void displayPositionAndDirection() {
+        cout << "Final Position: (" << currentPosition.xPos << ", " << currentPosition.yPos << ", " << currentPosition.zPos << ")\n";
+        cout << "Final Direction: ";
+        switch (currentOrientation) {
+            case North:
+                cout << "North\n";
+                break;
+            case South:
+                cout << "South\n";
+                break;
+            case East:
+                cout << "East\n";
+                break;
+            case West:
+                cout << "West\n";
+                break;
+            case Up:
+                cout << "Up\n";
+                break;
+            case Down:
+                cout << "Down\n";
+                break;
+        }
+    }
+};
+
+int main() {
+    // Create a spacecraft object with an initial position and direction
+    Spacecraft lunarCraft(0, 0, 0, North);
+
+    // Define a sequence of commands to be executed
+    vector<char> commands = {'f', 'r', 'u', 'b', 'l'};
+
+    // Execute the commands on the spacecraft
+    lunarCraft.executeCommands(commands);
+
+    // Display the starting position and direction, as well as the final position and direction
+    cout << "Starting Position: (0, 0, 0)\n";
+    cout << "Initial Direction: North\n";
+    lunarCraft.displayPositionAndDirection();
+
+    return 0;
+}
+
